18f75261603b7775c745778c13f55f1191bd673d
alexiuswijaya <48556962+alexiuswijaya@users.noreply.github.com>
2021-02-24 17:16:31 +0700

    Intent Activity Fragment

---
+++ WEEK 04/app/src/main/java/umn/ac/id/ActivityFirst.java
@@ -0,0 +1,13 @@
+package umn.ac.id;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityFirst extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
